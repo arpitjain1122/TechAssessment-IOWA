1ef484b70421df47d98ad721a1a456aec4797feb
arpit jain <jain.arpit@northeastern.edu>
2025-04-08 08:45:58 -0500

    Creating the bare bone backend

---
+++ receipt-backend/src/test/java/com/iowa/receipt_backend/ReceiptBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iowa.receipt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
